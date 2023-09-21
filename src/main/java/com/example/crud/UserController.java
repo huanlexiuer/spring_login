e8bc1f93b3684ffdae9029e59c3fe2f3d483013f
slider <308018381@qq.com>
2023-09-21 16:23:49 +0800

    add vip user

--- src/main/java/com/example/crud/UserController.java
+++ src/main/java/com/example/crud/UserController.java
@@ -26,6 +26,8 @@ public class UserController {
             System.out.println("普通用户登录成功");
         } else if ((dbUser.getName().equals("yia") && dbUser.getPassword().equals("yia")) ) {
             System.out.println("vip用户登录成功");
+        } else if ((dbUser.getName().equals("newadd") && dbUser.getPassword().equals("newadd")) ) {
+            System.out.println("vip用户登录成功");
         }
         else{
             System.out.println("登录失败，请检查账号密码");
